e8d2ee59d180ac5cd59b83a8d15e84ca51b47a80
oindrila7 <43373988+oindrila7@users.noreply.github.com>
2021-09-02 09:39:49 +0600

    upload

--- src/test/java/TestSeleniumJunit.java
+++ src/test/java/TestSeleniumJunit.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -32,14 +30,14 @@ public class TestSeleniumJunit {
         driver.get("https://demoqa.com");
         String title=driver.getTitle();
         System.out.println(title);
-        Assert.assertTrue(title.contains("ToolsQA"));
+        Assert.assertTrue(title.contains("Toolsqa"));
     }
 
     @Test
     public void checkIfElementExists() throws InterruptedException {
         driver.get("https://demoqa.com");
         //Boolean status=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@src='/images/ToolSqa.jpg']"))).isDisplayed();
-        Boolean status = driver.findElement(By.xpath("//img[@src='/images/ToolSqa.jpg']")).isDisplayed();
+        Boolean status = driver.findElement(By.xpath("//img[@src='/images/Toolsqa.jpg']")).isDisplayed();
         Assert.assertEquals(status,true);
     }
 
